cc6ed596c083c2836de4a2b30be713affeb099aa
seb776 <sebastien.maire@epitech.eu>
2015-02-20 00:55:29 +0100

    Integrated Exec on the fly

--- sources/Nodes/CodeNode.cpp
+++ sources/Nodes/CodeNode.cpp
@@ -9,14 +9,45 @@ using namespace System;
 
 generic<typename T>
 CodeNode<T>::CodeNode(String ^code) :
-	code_(code) {
+	code_(code), res_(nullptr) {
 }
 
 generic<typename T>
-void CodeNode<T>::Process(const T %value) {
-	System::Console::Write("[");
-	System::Console::Write(value);
-	System::Console::Write("=>");
-	System::Console::Write(this->code_);
-	System::Console::WriteLine("]");
+void CodeNode<T>::Build() {
+	CompilerResults ^last = res_;
+
+	res_ = CompilerConfig::provider->CompileAssemblyFromSource(CompilerConfig::compilerParams, code_);
+	if (res_->Errors->Count != 0) {
+		Console::WriteLine("Errors occured: getting back old code...");
+		for each (CompilerError ^ce in res_->Errors) {
+			Console::WriteLine(ce);
+		}
+		res_ = last;
+	}
+}
+
+generic<typename T>
+void CodeNode<T>::Process(T %value) {
+	Console::WriteLine("Called with ref");
+	if (res_) {
+		Object ^o = res_->CompiledAssembly->CreateInstance("Node");
+
+		MethodInfo ^mi = o->GetType()->GetMethod("Input");
+		array<Object ^> ^a = gcnew array<Tuple<T> ^>(2);
+		a[0] = Tuple::Create(value);
+		mi->Invoke(o, a);
+	}
+}
+
+generic<typename T>
+void CodeNode<T>::Process(T value) {
+	Console::WriteLine("called with scalar");
+	if (res_) {
+		Object ^o = res_->CompiledAssembly->CreateInstance("Node");
+
+		MethodInfo ^mi = o->GetType()->GetMethod("Input");
+		array<Object ^> ^a = gcnew array<Tuple<T> ^>(2);
+		a[0] = Tuple::Create(value);
+		mi->Invoke(o, a);
+	}
 }
--- sources/main.cpp
+++ sources/main.cpp
@@ -7,9 +7,23 @@
 #include "Nodes\CodeNode.hpp"
 int main()
 {
-	ANode<int> ^in = gcnew CodeNode<int>(gcnew String("int var;"));
+	//CompilerConfig::Init();
+	ANode<int> ^in = gcnew CodeNode<int>(gcnew String("	\
+namespace Foo											\
+{														\
+    public class Bar									\
+	{													\
+        public void SayHello()							\
+        {												\
+            System.Console.WriteLine(\"Hello World\");	\
+        }												\
+    };													\
+};														\
+	"));
 
 	in->Process(42);
+ANode<ANode<int> ^> ^a = gcnew CodeNode<ANode<int> ^>("toto");
+//a->Process(in); // specialisation (where T : class must be reference)
 	System::Console::ReadKey();
 	return 0;
 }
