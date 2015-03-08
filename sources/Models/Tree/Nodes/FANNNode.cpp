fda2714641b4445644cad68e923ce877b31ab413
isidore emmanuel <isidor_e@epitech.eu>
2015-03-08 17:24:03 +0100

    fannNode added

--- sources/Models/Tree/Nodes/FANNNode.cpp
+++ sources/Models/Tree/Nodes/FANNNode.cpp
@@ -3,20 +3,43 @@
 ** Contribs:
 */
 
-#include "C:\FANN\include\fann.h"
+
 #include "FANNNode.hpp"
 
 FANNNode::FANNNode():
-outnb_(0), generated_(false)
+outnb_(0), generated_(false), ann_(nullptr), output_(nullptr)
 {}
 
+FANNNode::~FANNNode()
+{
+	if (ann_ != nullptr)
+		fann_destroy(this->ann_);
+}
+
 void FANNNode::build()
-{}
+{
+	ann_ = fann_create_standard(3, 1, 3, children_->Count);
+	this->outnb_ = children_->Count;
+}
 
 generic<typename T>
 void FANNNode::Process(T value)
 {
+	int i = 0;
+	if (children_->Count == 0)
+		return;
 	if ((generated_ == false) || (outnb_ != children_->Count))
 		build();
-
+	*input_father_ = (fann_type)(value);
+	output_ = (ann_, input_father_);
+	for each (KeyValuePair<String^, ANode^> entry in children_)
+	{
+		if (output_[i] < 1)
+			++i;
+		else
+		{
+			entry.Value->Process<T>((T)output_[i]);
+			break;
+		}
+	}
 }
--- sources/Models/Tree/Nodes/FANNNode.hpp
+++ sources/Models/Tree/Nodes/FANNNode.hpp
@@ -7,18 +7,20 @@
 
 #include "ANode.hpp"
 #include "Controllers\OnTheFly\CompilerAttr.hpp"
-//#include "fan/toussa²/blablabla.h"//ajouter les fichiers fann au projet
+#include "C:\FANN\include\fann.h"
 
 public ref class FANNNode : public ANode{
 
 private:
 	int		outnb_;
 	bool	generated_;
-	//fann_type input_father_;
-	//fann_type 
+	struct fann *ann_;
+	fann_type *input_father_;
+	fann_type *output_;
 
 public:
 	FANNNode();
+	~FANNNode();
 	void build();
 	generic<typename T>
 	void Process(T value) override;
